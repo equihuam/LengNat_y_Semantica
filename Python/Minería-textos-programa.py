6e35ce142647b398df57208df80ec3ba0a6000cb
MiguelEq <equihuam@gmail.com>
2015-05-08 01:31:41 -0500

    Avance un poco en la extraxión de los títulos. Todavía tengo problemas con la combinación de títulos que se extienden por dos líneas

--- Python/Minería-textos-programa.py
+++ Python/Minería-textos-programa.py
@@ -1,57 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Editor de Spyder
+Minería de texto para capturar los textos de los resumenes
+del V Congreso Mexicano de Ecología (
+San Luis Potosí abril-2015
 
-Este es archivo temporal
+Autor: Miguel Equihua Zamora
+email: equihuam@gamil.com
+
 """
 
-# Inicializa la librería nltk
+# Inicializa la librerías
 import nltk
+import re
+import os # biblioteca de acceso a sistema operativo
 
-# descarga los anexos del libro de "Natural Language Processing"
+# Abrir el archivo y leerlo en "texto"
+# Los archivos de datos están en:
+# Software-github/LengNat_y_Semantica/Datos y textos
+os.getcwd() # directorio actual
+rutaTextos = "C:/Users/Miguel/Documents/0 Versiones/1 Personales/Software-github\LengNat_y_Semantica\Datos y textos"
+os.chdir(rutaTextos)
+archivosTexto = os.listdir(rutaTextos)
+archivo = open(archivosTexto[9],'r')
+texto = archivo.readlines()
 
-nltk.download()
+# Lee linea por linea en texto hasta encontrar "Presentación" para separar
+# el texto respectivo.
+presentacion = texto[148:164]
 
-# Ejemplo de búsqueda de palabras en contexto
-text1
-text1.concordance("monstrous")
-text1.concordance("sea")
-text4.concordance("environment")
-text4.concordance("earth")
-text4.concordance("clean")
+# Elimina saltos de línea y junta todo en una sola línea de texto
+espacioSep = " "
+presentacion = espacioSep.join(linea.strip() for linea in presentacion)
 
-# Ejemplo de búsqueda de términos "similares"
-text1.similar("monstrous")
+# Corta los bloques de resumenes de  interés 
+simposios = texto[1591:10608]
+orales = texto[10710:33040]
+carteles = texto[33150:60312]
 
-# Se puede buscar el contexto que tienen en común un conjunto de términos
-text2.common_contexts(["monstrous", "very"])
+# Limpieza y segmentación de los resumenes título, texto, palClv e ID
+# Empieza por eliminar marcas de formato y espacios ed relleno
+simposios = list((linea.strip("0123456789* \t\n") for linea in simposios))
+orales = list((linea.strip("0123456789* \t\n") for linea in orales))
+carteles = list((linea.strip("0123456789* \t\n") for linea in carteles))
 
-# Gráfica de dispersión de un términos. En qué posición aparecen en relación
-# con el inicio del documento en una colección de documentos pegados en uno.
-# Por ejemplo se puede explorar el uso de palabras en los últimos 220 años 
-# en un texto construido artificialmente uniendo los textos de los discursos
-# inaugurales de principio a fin uno detras de otro.
-text4.dispersion_plot(["climate", "environment", "health", "earth", "clean"])
+simp = espacioSep.join(linea.strip() for linea in simposios)
+s = re.sub("^[A-Z0-9ÁÉÍÓÚÑ ,:]+","\1{tit}" simp)
 
-# Longitud de un texto
-len(text3)
+"ID:" in simposios[25] 
 
-# Vocabulario de un texto
-set(text3)
-sorted(set(text3))
-len(set(text3))
-
-# Uso promedio de cada palabra
-len(text3)/len(set(text3))
-
-#Conteo del uso de una palabra o "token"
-text3.count("smote")
-
-# Definición de una función para calcular la probabilidad de uso de
-# palabras en un texto
-
-
-
-
+# Separa los títulos
+titulo = []
+lineasConsecutivas = False 
+for i in range(len(simposios)):
+    if simposios[i].isupper() and "ID:" not in simposios[i]:
+        if lineasConsecutivas:
+            texto = texto + " " + simposios[i]
+        titulo.append(simposios[i])  
+        lineasConsecutivas = not lineasConsecutivas
 
 
