4554cc89d98eb4379a016b6777c40b7295b71d30
MiguelEq <equihuam@gmail.com>
2015-05-08 20:35:18 -0500

    Ya recupera los títulos y los cuerpos de los resúmenes.  Todavía hay que verificar detalles pues los números de unos y otros on coinciden.

--- Python/Minería-textos-programa.py
+++ Python/Minería-textos-programa.py
@@ -18,11 +18,12 @@ import os # biblioteca de acceso a sistema operativo
 # Los archivos de datos están en:
 # Software-github/LengNat_y_Semantica/Datos y textos
 os.getcwd() # directorio actual
-rutaTextos = "C:/Users/Miguel/Documents/0 Versiones/1 Personales/Software-github\LengNat_y_Semantica\Datos y textos"
+rutaTextos = "C:/Users/Miguel/Documents/0 Versiones/1 Personales/Software-github/LengNat_y_Semantica/Datos y textos"
 os.chdir(rutaTextos)
 archivosTexto = os.listdir(rutaTextos)
 archivo = open(archivosTexto[9],'r')
 texto = archivo.readlines()
+archivo.close()
 
 # Lee linea por linea en texto hasta encontrar "Presentación" para separar
 # el texto respectivo.
@@ -43,19 +44,31 @@ simposios = list((linea.strip("0123456789* \t\n") for linea in simposios))
 orales = list((linea.strip("0123456789* \t\n") for linea in orales))
 carteles = list((linea.strip("0123456789* \t\n") for linea in carteles))
 
+
+# Concatenación de la lista en un solo bloque de texto corrido
 simp = espacioSep.join(linea.strip() for linea in simposios)
-s = re.sub("^[A-Z0-9ÁÉÍÓÚÑ ,:]+","\1{tit}" simp)
 
-"ID:" in simposios[25] 
+# Uso de expresiones regulares para extraer los bloques de interés
+# Selección del título
+# Es un conjunto de caracteres en mayúscula. Hay que evitar leer el año 
+# y la hora y unas NOM o NMX, que caben como un patrón válido, así como 
+# unos agradecimientos al PAPIIT (ya los borré). Hay algunos títulos que 
+# queda con una mayúscula suelta al final del texto. La r antes de las
+# comillas indica interpretación directa ("raw") del texto.
+regExp_sec = re.compile(r"(?! |IN207615|2015|,|[0-9]|AM|\n|[- ]{2,})([A-Z0-9ÁÉÍÓÚÑ:;¿\?\(\)\-\+\. ,]{25,})") 
+ms =  regExp_sec.findall(simp)
+titulosEnSimposios = list((linea[:-1].strip() for linea in ms))
 
-# Separa los títulos
-titulo = []
-lineasConsecutivas = False 
-for i in range(len(simposios)):
-    if simposios[i].isupper() and "ID:" not in simposios[i]:
-        if lineasConsecutivas:
-            texto = texto + " " + simposios[i]
-        titulo.append(simposios[i])  
-        lineasConsecutivas = not lineasConsecutivas
+# Elimino la mayúscula sueltaque queda al final de algunos títulos.
+for i in range(len(titulosEnSimposios)):
+    a = re.search(" +[A-Z]$", titulosEnSimposios[i])
+    if a:
+      titulosEnSimposios[i] = re.sub(a.group(0), "", titulosEnSimposios[i])
+
+# Recortar el bloque de texto
+# Está entre el correo electrónico del autor correspondiente e ID:
+regExp_sec = re.compile(r"([\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}") 
+regExp_sec = re.compile(r"(?<=@).*?(?= Palabras clave: )")
+textosEnSimposios =  regExp_sec.findall(simp)
 
 
