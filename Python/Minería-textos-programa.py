ba4ad05fac10f1c6ccf9ab025ba74e516ac70485
MiguelEq <equihuam@gmail.com>
2015-05-11 00:58:25 -0500

    Resumenes separados y depurados

--- Python/Minería-textos-programa.py
+++ Python/Minería-textos-programa.py
@@ -15,41 +15,88 @@ import re
 import os # biblioteca de acceso a sistema operativo
 import csv
 
-# Abrir el archivo y leerlo en "texto"
+# --------------------  Expresión Regular -----------------------------
+# Prepara el código de expresión regular para la selección del título
+# Es un conjunto de caracteres en mayúscula. Hay que evitar leer el año 
+# y la hora y unas NOM o NMX, que caben como un patrón válido, así como 
+# unos agradecimientos al PAPIIT (ya los borré).La r antes de las
+# comillas indica interpretación directa ("raw") del texto.
+
+# Gracias a consejo de sln en http://stackoverflow.com/questions/30142018
+# "(?<![A-Z0-9ÁÉÍÓÚÑ:;¿?()+.,])((?!(?:NOM-059-SEMARNAT|NOM-059 SEMARNAT 2010:|DAP-|GATA|NOM-059-2010-SEMARNAT))[A-ZÁÉÍÓÚÑ¿\"](?:(?!(?:[-]{2,}|-2010|GATA|NOM\-059\-SEMARNAT))[A-Z0-9ÁÉÍÓÚÑ&:;¿?()´\-+. ,\"]){19,}(?<=[A-ZÁÉÍÓÚÑ?&\"\-´ ]))(?![a-z])"
+
+# No es necesaria esta separación de componentes, pero es didáctico aquí!!!
+# Construcción de la expresión regular para recuperar títulos
+# Ve (hacia atras, sin seleccionar: "?<") - Que no ("!") inicie en medio de algo
+RE_capturaTitulo = (
+  r"(?<![A-Z0-9ÁÉÍÓÚÑÇÃ×:;¿?()+.,])" + # No hay nada antes del texto
+  r"(" + # Inicia la construcción del grupo 1
+     r"(?!" + # Ve hacia adelante que no inicie con nada como lo que sigue
+        r"(?:" + # Agrupa sin capturar lo que sigue (añadir lo necesario)
+           # Colección de términos y frases a omitir de la captura 
+           r"MATERIALES Y METODOS|MATERIALES Y MÉTODOS.|NOM-059-SEMARNAT|" +
+           r"NOM-059 SEMARNAT 2010:|DAP-|GATA|NOM-059-2010-SEMARNAT|ICRR|" + 
+           r"NOM- 059- ECOL-2001.|AS: 10-6M, 10-7M, 10-8|CONAFOR-CONACYT" +
+     r"))" + # Cierra la exploración y el grupo de cosas por evitar
+
+     # Define como empieza la captura: Inicia con una letra incluso con acento
+     # o comillas o abre interrogación
+     r"[A-ZÁÉÍÓÚÑÇÃ×¿\"]" + # Inicia la captura con alguno de estos caracteres
+
+     # Inicia el grupo de captura 
+     r"(?:"  + # agrupa sin capturar lo que sigue (al menos 19 veces)
+        # El grupo de captura no puede contener lo que se anota en este conjunto
+        r"(?!" + # ve hacia adelante que no haya lo que sigue
+           r"(?:" + # agrupa sin capturar lo que hay que evitar
+              r"[-]{2,}|-2010|GATA40|GATA16|IAN873,|IAN710,|RRIM600,|PB260" + 
+              r"NOM\-059\-SEMARNAT|NOM\- 059\- ECOL-2001.|ICRR" + # Omitir esto
+        r"))" + # cierra el grupo de cosas a evitar y el conjunto que lo define
+        r"[A-Z0-9ÁÉÍÓÚÑÇÃ×&:;¿?()´\-+. ,\"]" + # Esto es lo que hay que capturar
+     r"){19,}" + # Cierra agrupa 2 y captura repetida al menos 19 veces.
+   r"(?<=[A-ZÁÉÍÓÚÑÇÃ×?&\"\-´ ])" + # ve hacia atras que el grupo incluya alguno de estos
+   r")" + # Termina el grupo de captura 
+   r"(?![a-záéíóú])") # El patrón concluye al encontrar una minúscula tras la captura
+
+# ------------------- Localización de archivos de texto --------------------
 # Los archivos de datos están en:
 # Software-github/LengNat_y_Semantica/Datos y textos
+# Abrie el archivo y carga en "texto"
 os.getcwd() # directorio actual
-rutaTextos = "C:/Users/Miguel/Documents/0 Versiones/1 Personales/Software-github/LengNat_y_Semantica/Datos y textos"
-os.chdir(rutaTextos)
-archivosTexto = os.listdir(rutaTextos)
-archivo = open(archivosTexto[9],'r')
+rutaBase = "C:/Users/Miguel/Documents/0 Versiones/1 Personales/Software-github/"
+rutaTextos = "LengNat_y_Semantica/Datos y textos"
+os.chdir(rutaBase + rutaTextos)
+archivosTexto = os.listdir(rutaBase + rutaTextos)
+archivosTexto.index("memorias_en_edicion_doc.txt")
+archivo = open("memorias_en_edicion_doc.txt",'r')
 texto = archivo.readlines()
 archivo.close()
 
-# Lee linea por linea en texto hasta encontrar "Presentación" para separar
-# el texto respectivo.
-presentacion = texto[148:164]
+# ---------  Segmentación por bloques de contenido ---------------------
+# -------- presentación, simposios, orales y carteles ------------------
 
+# Corta los bloques de  interés 
+presentacion = texto[148:166]
+simposios = texto[1591:10611]
+orales = texto[10712:33043]
+carteles = texto[33152:60313]
+
+# ------------------------ Presentación  ---------------------------
 # Elimina saltos de línea y junta todo en una sola línea de texto
 espacioSep = " "
 presentacion = espacioSep.join(linea.strip() for linea in presentacion)
 
-# Corta los bloques de resumenes de  interés 
-simposios = texto[1591:10608]
-orales = texto[10710:33040]
-carteles = texto[33150:60312]
 
 # Limpieza y segmentación de los resumenes título, texto, palClv e ID
 # Empieza por eliminar marcas de formato y espacios ed relleno
-simposios = list((linea.lstrip("[0123456789* \t\n") for linea in simposios))
-orales = list((linea.lstrip("^0123456789* \t\n") for linea in orales))
-carteles = list((linea.lstrip("^0123456789* \t\n") for linea in carteles))
+simposios = [linea.lstrip("[0123456789* \t\n") for linea in simposios]
+orales = [linea.lstrip("^0123456789* \t\n") for linea in orales]
+carteles = [linea.lstrip("^0123456789* \t\n") for linea in carteles]
 
 # Hubo 33 sesiones de simposio
 # Se reportan los siguientes números de presentaciones:
 # 192 en simposio , 478 orales y 553 carteles
 # Pero recupero del documento de resumenes:
-# 186 en simposio, 
+# 186 en simposio, 451 orales y 546 carteles
 
 # --------------- Simposios ----------------
 
@@ -63,21 +110,13 @@ simpTexto = espacioSep.join(linea.strip() for linea in simposios)
 regExp_sec = re.compile(r"(ID:[ 0-9]+)") 
 simpID =  regExp_sec.findall(simpTexto)
 
-# Selección del título
-# Es un conjunto de caracteres en mayúscula. Hay que evitar leer el año 
-# y la hora y unas NOM o NMX, que caben como un patrón válido, así como 
-# unos agradecimientos al PAPIIT (ya los borré). Hay algunos títulos que 
-# queda con una mayúscula suelta al final del texto. La r antes de las
-# comillas indica interpretación directa ("raw") del texto.
-regExp_sec = re.compile(r"(?! |IN207615|2015|,|[0-9]|AM|\n|[- ]{4,})([A-Z0-9ÁÉÍÓÚÑ&:;¿\?\(\)\-\+\.\", ]{25,})") 
-simpTitulos =  regExp_sec.findall(simpTexto)
-simpTitulos = list((linea[:-1].strip() for linea in simpTitulos))
+# Recupera las palabras clave
+regExp_sec = re.compile(r"(?<=Palabras clave: ).*?(?=ID:)") 
+simpPalClv = regExp_sec.findall(simpTexto)
 
-# Elimino la mayúscula sueltaque queda al final de algunos títulos.
-for i in range(len(simpTitulos)):
-    a = re.search(" +[A-Z]$", simpTitulos[i])
-    if a:
-      simpTitulos[i] = re.sub(a.group(0), "", simpTitulos[i])
+# Selección del título: usa la expresión regular preparada al principio!
+regExp_sec = re.compile(RE_capturaTitulo) 
+simpTitulos =  regExp_sec.findall(simpTexto)
 
 # Recortar el bloque de texto
 # Está entre el correo electrónico del autor correspondiente e ID:
@@ -86,10 +125,6 @@ simpResumen =  regExp_sec.findall(simpTexto)
 
 # Elimina el residuo de email que quedó al inicio de los resumenes
 simpResumen = [re.sub("^[a-z\. ]+", "", t) for t in simpResumen]
-
-# Recupera las palabras clave
-regExp_sec = re.compile(r"(?<=Palabras clave: ).*?(?=ID:)") 
-simpPalClv = regExp_sec.findall(simpTexto)
 
 # Combina los datos en una lista anidada
 simpDatos = zip(simpID, simpTitulos, simpPalClv, simpResumen)
@@ -111,15 +146,66 @@ oralTexto = espacioSep.join(linea.strip() for linea in orales)
 regExp_sec = re.compile(r"(ID:[ 0-9]+)") 
 oralID =  regExp_sec.findall(oralTexto)
 
-# Selección del título
-#(?!(?:static|my|admin|www)$)
-# GATA|NOM\-059|2015|,|[0-9]|AM| |\n|[- ]{2,}
-# (?<![A-Z0-9ÁÉÍÓÚÑ:;¿?()\-+.,])((?!(?:NOM-059-SEMARNAT))[A-ZÁÉÍÓÚÑ¿](?:(?!(?:[- ]{2,}|NOM\-059\-SEMARNAT))[A-Z0-9ÁÉÍÓÚÑ:;¿?()\-+. ,]){9,}(?<=[A-ZÁÉÍÓÚÑ?]))(?![a-z])
-regExp_sec = re.compile(r"(?<![A-Z0-9ÁÉÍÓÚÑ:;¿?()+.,])((?!(?:NOM-059-SEMARNAT|NOM-059 SEMARNAT 2010:|DAP-|GATA|NOM-059-2010-SEMARNAT))[A-ZÁÉÍÓÚÑ¿\"](?:(?!(?:[-]{2,}|-2010|GATA|NOM\-059\-SEMARNAT))[A-Z0-9ÁÉÍÓÚÑ&:;¿?()´\-+. ,\"]){19,}(?<=[A-ZÁÉÍÓÚÑ?&\"\-´ ]))(?![a-z])") 
+#Extrae las palabras clave
+regExp_sec = re.compile(r"(?<=Palabras clave: ).*?(?=ID:)") 
+oralPalClv = regExp_sec.findall(oralTexto)
+
+# Recupera los títulos de los resúmenes
+regExp_sec = re.compile(RE_capturaTitulo)
 oralTitulos =  regExp_sec.findall(oralTexto)
-oralTitulos = list((linea[:-1].strip() for linea in oralTitulos))
+oralTitulos = [t.strip() for t in oralTitulos]
 
+# Extrae los textos de los resumenes
+regExp_sec = re.compile(r"(?<=@).*?(?= Palabras clave: )")
+oralResumen =  regExp_sec.findall(oralTexto)
 
-oralTitulos[346]
+# Elimina el residuo de email que quedó al inicio de los resumenes
+oralResumen = [re.sub("^[a-z\. ]+", "", t) for t in oralResumen]
 
+# Combina los datos en una lista anidada
+oralDatos = zip(oralID, oralTitulos, oralPalClv, oralResumen)
+oralDatos = [list(fila) for fila in oralDatos]
 
+# Escribe el bloque de orales al disco en formato csv
+with open("orales.csv", "wb") as f:
+        w = csv.writer(f, dialect = "excel-tab")
+        for fila in oralDatos:        
+           w.writerow(fila)
+
+# --------------- Carteles ----------------
+
+# Inicia en línea 33153 del documento fuente
+# Concatenado de los textos en un solo bloque corrido de orales
+cartelTexto = espacioSep.join(linea.strip() for linea in carteles)
+
+# Recupera el identificador del resumen 
+regExp_sec = re.compile(r"(ID:[ 0-9]+)") 
+cartelID =  regExp_sec.findall(cartelTexto)
+
+# Recupera las palabras clave
+regExp_sec = re.compile(r"(?<=Palabras clave: ).*?(?=ID:)") 
+cartelPalClv = regExp_sec.findall(cartelTexto)
+
+# Selección del título: usa la expresión regular preparada al principio!
+regExp_sec = re.compile(RE_capturaTitulo) 
+cartelTitulos =  regExp_sec.findall(cartelTexto)
+
+# Recortar el bloque de texto
+# Está entre el correo electrónico del autor correspondiente e ID:
+regExp_sec = re.compile(r"(?<=@).*?(?= Palabras clave: )")
+cartelResumen =  regExp_sec.findall(cartelTexto)
+
+# Elimina el residuo de email que quedó al inicio de los resumenes
+# Probelmas identificado: -noroeste.org (línes 258)  
+#                         165edu.mx (línea 95)
+cartelResumen = [re.sub("^[a-z0-9\-\. ]+", "", t) for t in cartelResumen]
+
+# Combina los datos en una lista anidada
+cartelDatos = zip(cartelID, cartelTitulos, cartelPalClv, cartelResumen)
+cartelDatos = [list(fila) for fila in cartelDatos]
+
+# Escribe el bloque de orales al disco en formato csv
+with open("carteles.csv", "wb") as f:
+        w = csv.writer(f, dialect = "excel-tab")
+        for fila in cartelDatos:        
+           w.writerow(fila)
